79531b54fe5d4072a705f06944534a7ed4ac66f2
David <info@pixelpro.es>
2020-05-21 12:26:54 +0200

    nuevo puerto

--- src/app/web.service.ts
+++ src/app/web.service.ts
@@ -7,6 +7,6 @@ export class WebService {
   constructor(private http: HttpClient){}
 
   getTask(){
-    return this.http.get('http://localhost:1234/tareas').toPromise();
+    return this.http.get('http://localhost:7070/api/tareas').toPromise();
   }
 }
