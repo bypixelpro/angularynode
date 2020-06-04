8aaeed122d1204c8bfcd992083e703ebd4657bad
David <info@pixelpro.es>
2020-06-04 12:18:30 +0200

    validacion 1

--- src/app/register.component.ts
+++ src/app/register.component.ts
@@ -1,25 +1,34 @@
 import { Component } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
+import { FormBuilder, Validators } from '@angular/forms';
 
 
 @Component({
   selector: 'register',
   templateUrl: 'register.component.html',
+  styles: [`
+   .error{
+     background-color: #ff4081;
+   }
+  `]
 })
 export class RegisterComponent {
   form: any;
   constructor(private fb: FormBuilder){
     this.form = fb.group({
-      nombre: '',
-      apellidos: '',
-      email: '',
-      password: '',
-      cpassword: ''
+      nombre: ['', Validators.required],
+      apellidos: ['', Validators.required],
+      email: ['', Validators.required],
+      password: ['', Validators.required],
+      cpassword: ['', Validators.required]
     });
   }
 
   onSubmit(){
     console.log(this.form.value);
+  }
+
+  isValid(control){
+    return this.form.controls[control].invalid && this.form.controls[control].touched;
   }
 
 }
