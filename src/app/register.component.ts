b966032966966229b069cca0ef338c2d3bddd283
David <info@pixelpro.es>
2020-06-10 10:47:14 +0200

    antes de auth

--- src/app/register.component.ts
+++ src/app/register.component.ts
@@ -17,18 +17,35 @@ export class RegisterComponent {
     this.form = fb.group({
       nombre: ['', Validators.required],
       apellidos: ['', Validators.required],
-      email: ['', Validators.required],
+      email: ['', [Validators.required, validEmail()]],
       password: ['', Validators.required],
       cpassword: ['', Validators.required]
-    });
+    }, {validator: isntSame('password', 'cpassword')});
   }
 
   onSubmit(){
-    console.log(this.form.value);
+    console.log(this.form.errors);
   }
 
   isValid(control){
     return this.form.controls[control].invalid && this.form.controls[control].touched;
   }
 
+}
+
+function isntSame(field1, field2){
+  return form => {
+    if (form.controls[field1].value !==  form.controls[field2].value){
+      return{ isntSame: true}
+    }
+  }
+}
+
+function validEmail(){
+  return control =>{
+    // tslint:disable-next-line:max-line-length
+    let regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+  return regex.test(control.value) ? null : {invalidEmail: true}
+}
 }
